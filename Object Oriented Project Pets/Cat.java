69101e09a6f02301565823b8e6d75bde80724edf
mikaylarmcgraw <50457687+mikaylarmcgraw@users.noreply.github.com>
2019-08-08 12:06:00 -0700

    Update Cat.java

--- Object Oriented Project Pets/Cat.java
+++ Object Oriented Project Pets/Cat.java
@@ -10,33 +10,20 @@ class Cat extends Pet
         Name = "Friskers";
         Activity = "playing with cat nip.";
         
-<<<<<<< HEAD
-=======
-        name = "Friskers";
-    
->>>>>>> c0b8e895b607fb4898e949c66a14e61669938e91
     }
     
     
     public void play()
     {
-<<<<<<< HEAD
         Mood = "Impressed";
 
         System.out.println("You gave "+Name+" cat nip!");
         
         super.play();
-=======
-        Mood = "Indifferent";
-        Activity = "playing with cat nip.";
-        
-        super.describeYourself();
->>>>>>> c0b8e895b607fb4898e949c66a14e61669938e91
         
         
     } 
     
-<<<<<<< HEAD
     public String speak()
     {
         
@@ -44,24 +31,9 @@ class Cat extends Pet
         
         
         return petSpeak;
-=======
-    public void play()
-    {
-        Mood = "Impressed";
-        
-        
-        System.out.println("You gave "+name+" cat nip!");
-        
-        super.play();
-        
->>>>>>> c0b8e895b607fb4898e949c66a14e61669938e91
         
     } 
     
     
 
-}    
-<<<<<<< HEAD
-    
-=======
->>>>>>> c0b8e895b607fb4898e949c66a14e61669938e91
+}   
