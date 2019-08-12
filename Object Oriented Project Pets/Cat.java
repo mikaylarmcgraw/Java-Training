5c51f512e18ae7e31c4c3f137d1aa1776e3a1cb0
mikaylarmcgraw <50457687+mikaylarmcgraw@users.noreply.github.com>
2019-08-12 09:30:14 -0700

    Update Cat.java

--- Object Oriented Project Pets/Cat.java
+++ Object Oriented Project Pets/Cat.java
@@ -34,6 +34,37 @@ class Cat extends Pet
         
     } 
     
+    public String speak(String petSpeak)
+    {
+        System.out.println("You chose to speak with "+Name);
+        String response = null;
+        switch (petSpeak)
+        {
+            case "hello":
+            response = "What do you need.... meow meow?";
+            break;
+            
+            case "praise":
+            System.out.println("You chose to praise your "+PetType);
+            response = "I know i'm a great "+PetType+" you're lucky to have me...";
+            break;
+            
+            case "discipline":
+            System.out.println("You chose to discipline your "+PetType);
+            response = "you'll move on... get me some cat nip and maybe it will motivate me to behave";
+            break;
+            
+            
+            default:
+            System.out.println("i'm sorry I don't know that command");
+            response = "I'm confussed...stupid human";
+            
+        }    
+        
+        return "Your pet says: "+response;
+
+    } 
+    
     
 
-}   
+}    
