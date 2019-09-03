266bf0ab002a5a571a43a957c71869e46485616f
mikaylarmcgraw <50457687+mikaylarmcgraw@users.noreply.github.com>
2019-09-03 14:00:06 -0700

    Create linkedListNode.java

---
+++ Linked List/linkedListNode.java
@@ -0,0 +1,35 @@
+public class linkedListNode
+{
+  //initalizing variables:
+  int name = 0;
+    
+    
+  public void serialNumber()
+  { 
+    
+    
+    
+  }
+  
+  public void nodeName(int menuOption)
+  {
+
+      if (menuOption == 1)
+      {
+         name++;
+         System.out.println("Your Node's new name is: "+name); 
+      }    
+      if (menuOption == 2)
+      {
+          name--;
+      }    
+          
+  }
+  
+  public void pointer()
+  { 
+    
+    
+    
+  }
+}    
