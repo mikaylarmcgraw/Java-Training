1bfd236f3103f8b3ba2db11d31963ad1b4832ad1
mikaylarmcgraw <50457687+mikaylarmcgraw@users.noreply.github.com>
2019-09-03 14:00:25 -0700

    Create linkedList.java

---
+++ Linked List/linkedList.java
@@ -0,0 +1,44 @@
+public class linkedList
+{
+    
+   linkedListNode Node = new linkedListNode();
+    
+   int length = 0;
+   
+   
+   
+    
+   public linkedList()
+   {
+       
+    
+   }
+    
+   public void addNodeToHead(linkedList myLinkedList,int test)
+   {
+       
+   
+   }
+   
+   public void addNodeToTail()
+   {
+   
+   
+    
+   }   
+   
+   public void removeNode()
+   {
+       
+       
+   }
+   
+   public void displayLinkedList()
+   {
+
+       
+       
+   }    
+   
+       
+}  
