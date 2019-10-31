54eb5516c60c94b501a5d44ff99315621dc9e9cb
mikaylarmcgraw <50457687+mikaylarmcgraw@users.noreply.github.com>
2019-10-31 14:30:49 -0700

    Update pyramid.java

--- Pyramid Problem/pyramid.java
+++ Pyramid Problem/pyramid.java
@@ -8,23 +8,46 @@ public class Pyramid
         
         //initalizing variables
         int pyramidLength = 0;
+        int loopCounter = 1;
         
         //creating scanner object
-        Scanner userInput=new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
         
         System.out.println("Please enter the length you'd like for your pyramid:");
         pyramidLength = userInput.nextInt();
         
         System.out.println("");
-        while (pyramidLength >= 1)
-        {
-            for (int i = pyramidLength; i > 1; i--)
+        
+        while (loopCounter <= pyramidLength)
+        { 
+            if (pyramidLength >= 10)
+            {
+                
+                for (int m = 0; m < (pyramidLength - loopCounter); m++)
+                {
+                    
+                    //System.out.print("+");
+                    
+                }    
+                
+                
+            }   
+            
+            
+            
+            for (int k = 0; k < pyramidLength - loopCounter; k++)
+            {
+                    System.out.print("   ");
+                    
+            }    
+            
+            for (int i = loopCounter; i > 1; i--)
             {
                 System.out.print(" " + i + " ");
                 
             }
             
-            for (int j = 1; j <= pyramidLength; j++)
+            for (int j = 1; j <= loopCounter; j++)
             {
                 System.out.print(" " + j + " ");
                 
@@ -32,9 +55,9 @@ public class Pyramid
             
 
             System.out.println("");
-            pyramidLength--;
+            loopCounter++;
         }
         
     }
 
-}   
+}    
