763a13225a716e804165e0f9b8458cb4981a1da9
mikaylarmcgraw <50457687+mikaylarmcgraw@users.noreply.github.com>
2019-11-06 09:05:58 -0800

    Update Order.java

--- Descending Strings/Order.java
+++ Descending Strings/Order.java
@@ -7,78 +7,86 @@ public class Order
         //no input error trapping necessary
         
         //initalizing variables
-        String citiesInputed = null;
         String cityOne = null;
         String cityTwo = null;
         String cityThree = null;
         String first = null;
         String second = null;
         String third = null;
-        
         //creating scanner object
         Scanner userInput = new Scanner(System.in);
         
-        System.out.println("This program will ask you to enter in three cities and then display them in decending order.");
-        System.out.println("When entering in cities please use a comma to seperate the cities with no extra spaces between entering them.");
-        System.out.println("Please enter the three cities now: ");
-        citiesInputed = userInput.nextLine();
-        
+        //capturing user input on cities entered
+        System.out.println("This program will ask you to enter in three cities (one at a time) and then will display them in decending order.");
+        System.out.println("Please enter first city: ");
+        cityOne = userInput.nextLine();
         System.out.println("");
-        
-        String[] cityArray = citiesInputed.split(",");
-        
-        cityOne = cityArray[0];
-        cityTwo = cityArray[1];
-        cityThree = cityArray[2];
+        System.out.println("Now please enter second city: ");
+        cityTwo = userInput.nextLine();
+        System.out.println("");
+        System.out.println("Lastly, please enter third and final city: ");
+        cityThree = userInput.nextLine();
+        System.out.println("");
+
+           //displaying cities entered:
+           System.out.println("Here are the cities you entered: " + cityOne+", " + cityTwo + ", " + cityThree);
+           System.out.println("");
             
-        char[]cityOneArray = cityOne.toCharArray();
-        char[]cityTwoArray = cityTwo.toCharArray();
-        char[]cityThreeArray = cityThree.toCharArray();
-        
-        for (int i = 0; i <cityOneArray.length;)
-        {
-            if (cityOneArray[i] > cityTwoArray[i])
+            if(cityOne.compareTo(cityTwo) > 0 && cityOne.compareTo(cityThree) > 0)
             {
                 first = cityOne;
-                i = cityOneArray.length;
+                
+                if (cityTwo.compareTo(cityThree) > 0)
+                {
+                    second = cityTwo;
+                    third = cityThree;
+                    
+                }   
+                else
+                {
+                    second = cityThree;
+                    third = cityTwo;
+                }    
             }    
-            else if (cityOneArray[i] == cityTwoArray[i])
-            {
-                i++;
-            }    
-            else 
+            else if (cityTwo.compareTo(cityOne) > 0 && cityTwo.compareTo(cityThree) > 0)
             {
                 first = cityTwo;
-                i = cityOneArray.length;
+                
+                if (cityOne.compareTo(cityThree) > 0)
+                {
+                    second = cityOne;
+                    third = cityThree;
+                }    
+                else
+                {
+                    second = cityThree;
+                    third  = cityOne;  
+                }    
             }    
-        }    
-        
-        for (int j = 0; j < cityThreeArray.length;)
-        {
-            if (cityTwoArray[j] > cityThreeArray[j]&& first == cityOne)
+            else if (cityThree.compareTo(cityOne) > 0 && cityThree.compareTo(cityTwo) > 0)
             {
-                second = cityTwo;
-                third = cityThree;
-                j = cityOneArray.length;
+                first = cityThree;
+                
+                if (cityOne.compareTo(cityTwo) > 0)
+                {
+                    second = cityOne;
+                    third = cityTwo;
+                }  
+                else
+                {
+                    second = cityTwo;
+                    third = cityOne;
+                    
+                }
+                
             }    
-            else if (cityTwoArray[j] == cityThreeArray[j])
-            {
-                j++;
-            }    
-            else 
-            {
-                second = cityThree;
-                third = cityTwo;
-                j = cityOneArray.length;
-            } 
-            
-            
-        }    
-        
+    
+        //displaying them in descending order
+        System.out.println("In descending order here is the list of cities: ");
         System.out.println(first);
         System.out.println(second);
         System.out.println(third);
-        
+
         
     }
 
