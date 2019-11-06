6a4a79e06001f032bfb7ca8b50815fc746c0b566
mikaylarmcgraw <50457687+mikaylarmcgraw@users.noreply.github.com>
2019-11-06 11:31:04 -0800

    Create Count.java

---
+++ Occurrence Problem/Count.java
@@ -0,0 +1,20 @@
+import java.util.*; 
+  
+public class Count
+{ 
+    public static void main(String args[]) 
+    { 
+        //initaizing variables
+        String numberInput = null;
+        
+        //creating scanner object
+        Scanner userInput = new Scanner(System.in);
+        
+        System.out.println("Please enter a string of numbers seperated with commas.");
+        System.out.println("Please don't add any extra spaces when entering in numbers.");
+        numberInput = userInput.nextLine();
+        
+   
+
+    }
+}
