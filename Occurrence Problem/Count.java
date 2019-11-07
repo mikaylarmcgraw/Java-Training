1a666fb7d5c9f2a1b20f31816bd7ad50002079ac
mikaylarmcgraw <50457687+mikaylarmcgraw@users.noreply.github.com>
2019-11-07 11:49:16 -0800

    Update Count.java

--- Occurrence Problem/Count.java
+++ Occurrence Problem/Count.java
@@ -6,36 +6,46 @@ public class Count
     { 
         //initaizing variables
         String numberInput = null;
-        String max = null;
+        int max = 0;
         int arrayLength = 0;
+        int occurrence = 0;
         //creating scanner object
         Scanner userInput = new Scanner(System.in);
         
-        System.out.println("Please enter a string of numbers seperated with commas.");
+        System.out.println("Please enter a string of numbers seperated with spaces.");
         System.out.println("Please don't add any extra spaces when entering in numbers.");
         numberInput = userInput.nextLine();
         
         System.out.println();
-
-        String[]arrayOfNumbers = numberInput.split(",");
+        
+        String[]arrayOfNumbers = numberInput.split(" ");
         arrayLength = arrayOfNumbers.length-1;
         
-        for (int i = 0; i < arrayLength; i++)
+        System.out.println("Here are the numbers you entered in: " + numberInput);
+        
+        for (int i = 1; i <= arrayLength; i++)
         {
-            if (arrayOfNumbers[i].compareTo(arrayOfNumbers[i+1]) < 0)
+            
+            if (Integer.parseInt((arrayOfNumbers[max])) < Integer.parseInt((arrayOfNumbers[i])))
             {
-                 max = arrayOfNumbers[i+1];
+                 max = i;
                     
             }  
-            else
-            {
-                max = arrayOfNumbers[i];
-                    
-            }    
-            
-   
+
             
         }   
-        System.out.println(max);
+        System.out.println("The maximum from the list of numbers entered is: " + arrayOfNumbers[max]);
+        
+        for (int j = 0; j <=arrayLength; j++)
+        {
+            if (Integer.parseInt((arrayOfNumbers[max])) == Integer.parseInt((arrayOfNumbers[j])))
+            {
+                occurrence++;
+            }
+            
+        }    
+        
+        System.out.println("It occurs: " + occurrence + " times.");
+        
     }
 }
