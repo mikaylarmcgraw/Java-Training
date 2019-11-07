9cfe5ba772374e14cffafd98e438c07481b0678e
mikaylarmcgraw <50457687+mikaylarmcgraw@users.noreply.github.com>
2019-11-06 19:51:21 -0800

    Update Count.java

--- Occurrence Problem/Count.java
+++ Occurrence Problem/Count.java
@@ -6,7 +6,8 @@ public class Count
     { 
         //initaizing variables
         String numberInput = null;
-        
+        String max = null;
+        int arrayLength = 0;
         //creating scanner object
         Scanner userInput = new Scanner(System.in);
         
@@ -14,7 +15,27 @@ public class Count
         System.out.println("Please don't add any extra spaces when entering in numbers.");
         numberInput = userInput.nextLine();
         
-   
+        System.out.println();
 
+        String[]arrayOfNumbers = numberInput.split(",");
+        arrayLength = arrayOfNumbers.length-1;
+        
+        for (int i = 0; i < arrayLength; i++)
+        {
+            if (arrayOfNumbers[i].compareTo(arrayOfNumbers[i+1]) < 0)
+            {
+                 max = arrayOfNumbers[i+1];
+                    
+            }  
+            else
+            {
+                max = arrayOfNumbers[i];
+                    
+            }    
+            
+   
+            
+        }   
+        System.out.println(max);
     }
 }
